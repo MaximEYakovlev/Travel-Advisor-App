e9efe9c1ac947964bd2dfbb259562848115688d1
Maxim Yakovlev <it.trainee@yandex.ru>
2022-04-10 20:23:10 +0300

    Map.jsx & styles.js: added styles

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,3 +1,11 @@
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab/Rating";
+import useStyles from "./styles.js";
+
 export const Map = () => {
+  const classes = useStyles();
+
   return <h1>Map</h1>;
 };
