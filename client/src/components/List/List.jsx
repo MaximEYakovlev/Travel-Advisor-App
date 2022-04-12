aaac9d68d28e9ee3b469982e65c624497ba77d8a
Maxim Yakovlev <it.trainee@yandex.ru>
2022-04-12 22:19:43 +0300

    added styles for List.jsx

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -1,3 +1,16 @@
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+import useStyles from "./styles.js";
+
 export const List = () => {
+  const classes = useStyles();
+
   return <h1>List</h1>;
 };
