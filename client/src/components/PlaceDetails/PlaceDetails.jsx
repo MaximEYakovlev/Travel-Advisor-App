d4be62a6b570fbb5156144de331f4183f56d7792
Maxim Yakovlev <it.trainee@yandex.ru>
2022-04-18 22:15:58 +0300

    components / PlaceDetails: added styles into the styles.js; imported elements from mui.com into the PlaceDetails.jsx

--- client/src/components/PlaceDetails/PlaceDetails.jsx
+++ client/src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,3 +1,18 @@
+import {
+  Box,
+  Typography,
+  Button,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Chip,
+} from "@material-ui/core";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import PhoneIcon from "@material-ui/icons/Phone";
+import Rating from "@material-ui/lab/Rating";
+import useStyles from "./styles.js";
+
 export const PlaceDetails = ({ place }) => {
   return <h1>{place.name}</h1>;
 };
