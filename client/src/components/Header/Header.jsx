28a4df37d69a05708b7575430460edad5603fab8
Maxim Yakovlev <it.trainee@yandex.ru>
2022-05-02 22:51:21 +0300

    added: set up a search for places

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,9 +2,19 @@ import { Autocomplete } from "@react-google-maps/api";
 import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import useStyles from "./styles.js";
+import { useState } from "react";
 
-export const Header = () => {
+export const Header = ({ setCoordinates }) => {
   const classes = useStyles();
+  const [autocomplete, setAutocomplete] = useState(null);
+
+  const onLoad = (autoC) => setAutocomplete(autoC);
+
+  const onPlaceChanged = () => {
+    const lat = autocomplete.getPlace().geometry.location.lat();
+    const lng = autocomplete.getPlace().geometry.location.lng();
+    setCoordinates({ lat, lng });
+  };
 
   return (
     <AppBar position="static">
@@ -16,7 +26,7 @@ export const Header = () => {
           <Typography variant="h6" className={classes.title}>
             Explore new places
           </Typography>
-          {/* <Autocomplete> */}
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
             <div className={classes.search}>
               <div className={classes.searchIcon}>
                 <SearchIcon />
@@ -26,7 +36,7 @@ export const Header = () => {
                 classes={{ root: classes.inputRoot, input: classes.inputInput }}
               />
             </div>
-          {/* </Autocomplete> */}
+          </Autocomplete>
         </Box>
       </Toolbar>
     </AppBar>
